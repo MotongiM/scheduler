706a3f63ea404c1eb3e86e105926d71123f1fd32
Michel <Michel_motongi@hotmail.com>
2022-01-18 17:45:20 -0500

    Fixed my classNames so the colors appear

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import classNames from "classnames";
+import "components/Button.scss"
 
 export default function Button(props) {
-  let buttonClass = "button";
-  classNames("button", {
-     "button--confirm":props.confirm,
-      "button--danger":props.danger
+  let buttonClass = classNames('button', {
+     'button--confirm': props.confirm,
+      'button--danger': props.danger
   });
 
   return ( 
