98d7ebc7a9f9b47e18245e22f79d8e066ae8b8eb
Michel <Michel_motongi@hotmail.com>
2022-01-20 01:32:19 -0500

    Added appointments data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
-
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -21,8 +21,47 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
 export default function Application(props) {
-  
+  const [day,setDay] = useState("Monday")
   return (
     <main className="layout">
         <section className="sidebar">
@@ -47,7 +86,9 @@ export default function Application(props) {
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments}
+        <Appointment key={appointment.id} {...appointment} />
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
