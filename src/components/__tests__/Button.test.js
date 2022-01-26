07dc2de5adada6640b9a6c25619d9f698a2b77ef
Michel <Michel_motongi@hotmail.com>
2022-01-26 12:39:25 -0500

    Added additional test for each button

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -4,6 +4,8 @@ import { render, cleanup } from "@testing-library/react";
 
 import Button from "components/Button";
 
+import { fireEvent } from "@testing-library/react";
+
 afterEach(cleanup);
 
 it("renders without crashing", () => {
@@ -15,3 +17,40 @@ it("renders its `children` prop as text", () => {
   expect(getByText("Default")).toBeInTheDocument();
 });
 
+it("renders a confirm button", () => {
+  const { getByText } = render(<Button confirm>Confirm</Button>);
+  expect(getByText("Confirm")).toHaveClass("button--confirm");
+});
+
+it("renders a danger button", () => {
+  const { getByText } = render(<Button danger>Danger</Button>);
+  expect(getByText("Danger")).toHaveClass("button--danger");
+});
+
+it("renders a clickable button", () => {
+  const handleClick = jest.fn();
+  const { getByText } = render(
+    <Button onClick={handleClick}>Clickable</Button>
+  );
+
+  const button = getByText("Clickable");
+
+  fireEvent.click(button);
+
+  expect(handleClick).toHaveBeenCalledTimes(1);
+});
+
+it("renders a disabled button", () => {
+  const handleClick = jest.fn();
+  const { getByText } = render(
+    <Button disabled onClick={handleClick}>
+      Disabled
+    </Button>
+  );
+
+  const button = getByText("Disabled");
+
+  fireEvent.click(button);
+
+  expect(handleClick).toHaveBeenCalledTimes(0);
+});
