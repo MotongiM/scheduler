c41c75cfd9eacb6376f098ac849c3651933a4ec7
Michel <Michel_motongi@hotmail.com>
2022-01-26 05:13:10 -0500

    Added getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,7 @@
 
 export function getAppointmentsForDay (state,day) {
    const filteredDays = state.days.filter((singleDay) => {
-       return singleDay.name === day;
+       return singleDay.name === day
    })
    if (filteredDays.length === 0) {
        return [];
@@ -11,3 +11,23 @@ export function getAppointmentsForDay (state,day) {
    })
    return appointmentMapped;
 };
+export function getInterviewersForDay(state, day) {
+  const filteredDay = state.days.find(dayOfDays => dayOfDays.name === day);
+  if (!state.days.length || !filteredDay) {
+    return [];
+  } else {
+    return filteredDay.interviewers.map((id) => state.interviewers[id])
+  }
+};
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+
+  const interviewerInfo = state.interviewers[interview.interviewer];
+  return {
+    "student": interview.student,
+    "interviewer": interviewerInfo
+  }
+}
