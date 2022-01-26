be2d22c74f2680224ae1ab850d76226a42e91b7b
Michel <Michel_motongi@hotmail.com>
2022-01-26 05:11:58 -0500

    Added visualMode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
+
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
 import "components/Appointment/style.scss"
-import Header from './Header';
-import Show from './Show';
-import Empty from './Empty';
+import useVisualMode from 'hooks/useVisualMode';
 
 export default function Appointment(props) {
-    return (
-        <article className="appointment">
-            <Header time={props.time} />
-            { props.interview ? 
-            <Show student={props.interview.student} interviewer={props.interview.interviewer} /> 
-            : 
-            <Empty/> 
-            }
-        </article>
-    )
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
+const { mode, transition, back } = useVisualMode (
+  props.interview ? SHOW : EMPTY
+);
+
+  return (
+
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+       <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+       />
+      )}  
+      {mode === CREATE && <Form name={props.name} value={props.value} interviewers={props.interviewers} onCancel={back}/>}
+    </article>
+  )
 }
