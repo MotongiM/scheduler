4d58a6998ce29bb717f46043f6bcf58ba3c41070
Michel <Michel_motongi@hotmail.com>
2022-01-26 06:27:56 -0500

    Cleaning up my code

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import {getAppointmentsForDay , getInterview , getInterviewersForDay}  from "../helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 
 
