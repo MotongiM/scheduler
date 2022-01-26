7ae36551c57ba57dfa8d4b86c7d4a8e8beb7cd48
Michel <Michel_motongi@hotmail.com>
2022-01-26 05:12:29 -0500

    Reverted change to make the code work

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,10 +6,9 @@ export default function DayList(props){
   return(
       <DayListItem
         key={dayObj.id}
-        name={dayObj.name}
-        spots={dayObj.spots}
-        selected={dayObj.name === props.value}
-        setDay={props.onChange}
+        selected={dayObj.name === props.day}
+        setDay={props.setDay}
+        {...dayObj}
         
         />
     )
